8f1e1226d7170aa5a2e7780a9a0c76f8983e43e9
michael catchen <michael.catchen@colorado.edu>
2021-07-30 13:39:57 -0400

    aight

--- src/00_probability_of_true_negative.jl
+++ src/00_probability_of_true_negative.jl
@@ -2,7 +2,10 @@ using Plots
 using Distributions
 using ColorSchemes
 using Colors
+using Measures
 numtimesobserved = 0:15
+
+fnt = font(20, "Computer Modern")
 
 truenegativeprob_fnr10  = []
 truenegativeprob_fnr25  = []
@@ -23,22 +26,22 @@ end
 
 
 cs = ColorSchemes.Paired_5
-plt = plot(legend=:outerright, xlim=numtimesobserved, legendtitle="false negative rate", ylim=(0,1), frame=:box, size=(800,400), dpi=300)
+plt = plot(fonyfamily=fnt, legendfontfamily=fnt,legend=:outerright, xlim=numtimesobserved, legendtitle="false negative rate", ylim=(0,1), frame=:box, size=(600,400), dpi=300)
 
-function add!(plt, x,y,sc)
-    plot!(plt, x, y, lw=3, lc=col(sc),label="$(sc)")
-    scatter!(plt, x, y, ms=5, msw=1.5, msc=col(sc), mc=:white, label="")    
+function add!(plt, x,y,val, ct)
+    plot!(plt, x, y, lw=3, lc=cs[ct],label="$(val)")
+    scatter!(plt, x, y, ms=5, msw=1.5, msc=cs[ct], mc=:white, label="")    
 end
-xaxis!("Number of observed negatives")
+xaxis!("Number of observed negatives", fontfamily=fnt)
 yaxis!("Probability interaction is a true negative")
 
 xticks!(0:15)
 plot!(plt, xlims=(0,15))
-add!(plt, numtimesobserved, truenegativeprob_fnr10, 0.1)
-add!(plt, numtimesobserved, truenegativeprob_fnr25, 0.25)
-add!(plt, numtimesobserved, truenegativeprob_fnr50, 0.5)
-add!(plt, numtimesobserved, truenegativeprob_fnr75, 0.75)
-add!(plt, numtimesobserved, truenegativeprob_fnr90, 0.9)
+add!(plt, numtimesobserved, truenegativeprob_fnr10, 0.1, 1)
+add!(plt, numtimesobserved, truenegativeprob_fnr25, 0.25, 2)
+add!(plt, numtimesobserved, truenegativeprob_fnr50, 0.5, 3)
+add!(plt, numtimesobserved, truenegativeprob_fnr75, 0.75, 4)
+add!(plt, numtimesobserved, truenegativeprob_fnr90, 0.9,5 )
 plt
 
 
---
+++ src/03_network_properties_empirical.jl
@@ -0,0 +1,103 @@
+using EcologicalNetworks: degree_out, degree_in
+using EcologicalNetworks
+using Distributions
+using DataFrames
+using StatsBase
+using Plots
+using SparseArrays
+
+function observe(A::T; falsepositive=0.1, falsenegative=0.3) where {T <: BipartiteNetwork}
+    mat = Matrix(A.edges)
+    for (i,el) in enumerate(mat)
+        if el == 1 && rand() < falsenegative
+            mat[i] = 0 
+        elseif el == 0 && rand() < falsepositive
+            mat[i] = 1
+        end
+    end
+    return T(sparse(mat), A.T, A.B)
+end
+
+function observe(A::T; falsepositive=0.1, falsenegative=0.3) where {T <: UnipartiteNetwork}
+    mat = Matrix(A.edges)
+    for (i,el) in enumerate(mat)
+        if el == 1 && rand() < falsenegative
+            mat[i] = 0 
+        elseif el == 0 && rand() < falsepositive
+            mat[i] = 1
+        end
+    end
+    return UnipartiteNetwork(mat)
+end
+
+function generate(truenet; richness=30, connectance=0.3, forbidden=nothing, kw...)
+    O = observe(truenet; kw...)
+
+    return truenet,O
+end
+
+function meandegcentrallity(net) 
+    mean(values(centrality_degree(net)))
+end
+
+function entropy(net)
+    return EcologicalNetworks.entropy(net)
+end
+
+function samplenetworks(A, fpr, fnr, property; numreplicates = 200)
+    realstat = zeros(numreplicates)
+    obstat = zeros(numreplicates)
+    for r in 1:numreplicates
+        metaweb = typeof(A) <: Function ? A(1) : A             
+
+        real, obs = generate(metaweb, falsepositive=fpr,falsenegative=fnr)    
+        realstat[r] = property(real)
+        obstat[r] = property(obs)
+    end
+    
+    error = sqrt.((realstat .- obstat).^2)
+    return error
+end
+
+function get_error(netlist, fp, property)
+    fnr = []
+    meanerr = []
+    err1σ = []
+    fn_continuum = 0.0:0.02:1
+     
+    @showprogress for fn in fn_continuum
+        thiserrs = []
+        thissigmas = []
+        for net in netlist
+            error = samplenetworks(net, fp, fn, property)
+            push!(thiserrs, mean(error))
+            push!(thissigmas, sqrt(var(error)))
+        end
+        push!(fnr, fn)
+        push!(meanerr, mean(thiserrs))
+        push!(err1σ, mean(thissigmas))
+    end
+    return fnr, meanerr, err1σ
+end
+#=
+
+    Running on MANGAL NETWORKS SECITON 
+
+=#
+include("get_mangal_data.jl")
+
+fw, para, mutu, misc = mangaldata()
+
+fpr = 0
+fnr, para_err, para_sigma = get_error(para, fpr, η)
+fnr, mutu_err, mutu_sigma = get_error(mutu, fpr, η)
+
+
+pl = plot()
+for (mn,sg) in zip(mutu_errs, mutu_sigmas)
+    plot!(pl, fn_continuum, mn, c=:red)
+end
+for (mn,sg) in zip(para_errs, para_sigmas)
+    plot!(pl, fn_continuum, mn, c=:blue)
+end
+pl
